065e410306e746bf2b20da6619e9e78611a46b17
mike belanger <mikejamesbelanger@gmail.com>
2025-04-12 17:10:38 -0400

    Renamed SomeElement -> TodoList because that is a reasonable thing to call it

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
 const ListItem = (item, id) => {
-    const el = document.createElement('li');
-    el.textContent = item;
-    el.id = id;
-    return el;
-}
+  const el = document.createElement("li");
+  el.textContent = item;
+  el.id = id;
+  return el;
+};
 
-class SomeElement extends HTMLElement {
-    static observedAttributes = ["message"]
-    constructor() {
-        super();
-        this.message = "";
-        this.items = [];
-        this.removeCallback = (name, id) => {};
-    }
-    render() {
-        this.innerHTML = '';
-        this.items.forEach((item, index) => {
-            const li = ListItem(item.name, item.id);
-            li.addEventListener('click', () => {
-                this.items.splice(index, 1);
-                this.removeCallback(item.id);
-                this.render();
-            });
-            this.appendChild(li);
-        });
-    }
-    connectedCallback() {
+class TodoList extends HTMLElement {
+  static observedAttributes = ["message"];
+  constructor() {
+    super();
+    this.message = "";
+    this.items = [];
+    this.removeCallback = (name, id) => {};
+  }
+  render() {
+    this.innerHTML = "";
+    this.items.forEach((item, index) => {
+      const li = ListItem(item.name, item.id);
+      li.addEventListener("click", () => {
+        this.items.splice(index, 1);
+        this.removeCallback(item.id);
         this.render();
+      });
+      this.appendChild(li);
+    });
+  }
+  connectedCallback() {
+    this.render();
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "message") {
+      this.message = newValue;
     }
-    attributeChangedCallback(name, oldValue, newValue) {
-        if (name === 'message') {
-            this.message = newValue;
-        }
-    }
-    addItem(name, id) {
-        this.items.push({ name, id });
-    }
-    setRemoveCallback(fn) {
-        this.removeCallback = fn;
-    }
+  }
+  addItem(name, id) {
+    this.items.push({ name, id });
+  }
+  setRemoveCallback(fn) {
+    this.removeCallback = fn;
+  }
 }
 
-export default SomeElement;
+export default TodoList;
