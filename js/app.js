c02f441313982388000f4e5a0fe743659c22faff
mike belanger <mikejamesbelanger@gmail.com>
2026-04-18 18:12:22 -0400

    Remove fieldset in web component container as it does not really make sense, and added unnecessary complexity.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const randomID = () => {
   const randomTime = new Date().getTime();
-  return `${Math.random() * randomTime}`;
+  return `${Math.floor(Math.random() * randomTime)}`;
 };
 
 const setAttributes = (element, attributes) => {
@@ -28,40 +28,30 @@ class TodoList extends HTMLElement {
   constructor() {
     super();
     this.checkTodoCallback = (id, done) => {};
-    this.rootID = randomID();
   }
   render(items = []) {
-    this.innerHTML = this.generateRoot(this.rootID).outerHTML;
     items.forEach((item, index) => {
       const todo = this.newTodo(item.name, item.id, item.done);
-      this.getRoot().appendChild(todo);
+      this.appendChild(todo);
     });
   }
   connectedCallback() {
     this.render();
   }
-  generateRoot(id) {
-    const ul = document.createElement("fieldset");
-    ul.setAttribute("id", id);
-    return ul;
-  }
-  getRoot() {
-    return this.children[this.rootID];
-  }
   addTodo(name, id, done) {
     const newTodo = this.newTodo(name, id, done);
-    this.getRoot().appendChild(newTodo);
+    this.appendChild(newTodo);
   }
   updateTodo(name, id, done) {
-    const currentItem = this.getRoot().children[id];
+    const currentItem = this.children[id];
     const newItem = this.newTodo(name, id, done);
-    this.getRoot().replaceChild(newItem, currentItem);
+    this.replaceChild(newItem, currentItem);
   }
   removeTodo(id) {
-    const targetChild = this.getRoot().children[id];
+    const targetChild = this.children[id];
 
     if (targetChild) {
-      this.getRoot().removeChild(targetChild);
+      this.removeChild(targetChild);
     }
   }
   setCheckTodoCallback(fn) {
@@ -82,7 +72,7 @@ class TodoList extends HTMLElement {
     return checkboxLabelContainer;
   };
   removeDoneTodos(cb) {
-    Array.from(this.getRoot().children).forEach((n) => {
+    Array.from(this.children).forEach((n) => {
       if (n.control.checked) {
         cb(n.id);
         this.removeTodo(n.id);
@@ -90,9 +80,7 @@ class TodoList extends HTMLElement {
     });
   }
   getCheckedItems = () => {
-    return Array.from(this.getRoot().getElementsByTagName("input")).map(
-      (x) => x.checked,
-    );
+    return Array.from(this.getElementsByTagName("input")).map((x) => x.checked);
   };
 }
 
